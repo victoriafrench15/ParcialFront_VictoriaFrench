6dd6cbb5171b160349c86fb0d4c81bce2a65e6d8
victoriaFrench <vicky.french@hotmail.com>
2023-03-07 22:12:14 -0300

    segundo commit - componentes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './components/Card';
+import './components/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+      Hola
+    </div>
+  );
+}
+
+export default App;
